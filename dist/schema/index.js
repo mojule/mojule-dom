5dfe7ec586965ba6430a7abbb715c796145945c9
Nik Coughlin <nrkn.com@gmail.com>
2017-01-09 16:48:41 +1300

    Move schema
    They don't need to be babelified and aren't copied by gulp anyway
    
    Also removed now unused emptynodevalue schema

--- dist/schema/index.js
+++ dist/schema/index.js
@@ -3,28 +3,27 @@
 var entityNode = require('mtype-node/schema/entityNode.schema.json');
 var entityNodeValue = require('mtype-node/schema/entityNodeValue.schema.json');
 var emptyNode = require('mtype-node/schema/emptyNode.schema.json');
-var emptyNodeValue = require('mtype-node/schema/emptyNodeValue.schema.json');
 var parentNode = require('mtype-node/schema/parentNode.schema.json');
 
-var comment = require('./comment.schema.json');
-var commentValue = require('./commentValue.schema.json');
-var documentFragment = require('./documentFragment.schema.json');
-var documentFragmentValue = require('./documentFragmentValue.schema.json');
-var document = require('./document.schema.json');
-var documentValue = require('./documentValue.schema.json');
-var documentType = require('./documentType.json');
-var documentTypeValue = require('./documentTypeValue.schema.json');
-var element = require('./element.schema.json');
-var elementValue = require('./elementValue.schema.json');
-var text = require('./text.schema.json');
-var textValue = require('./textValue.schema.json');
+var comment = require('../../schema/comment.schema.json');
+var commentValue = require('../../schema/commentValue.schema.json');
+var documentFragment = require('../../schema/documentFragment.schema.json');
+var documentFragmentValue = require('../../schema/documentFragmentValue.schema.json');
+var document = require('../../schema/document.schema.json');
+var documentValue = require('../../schema/documentValue.schema.json');
+var documentType = require('../../schema/documentType.json');
+var documentTypeValue = require('../../schema/documentTypeValue.schema.json');
+var element = require('../../schema/element.schema.json');
+var elementValue = require('../../schema/elementValue.schema.json');
+var text = require('../../schema/text.schema.json');
+var textValue = require('../../schema/textValue.schema.json');
 
 var schema = {
   comment: comment, commentValue: commentValue, documentFragment: documentFragment,
   documentFragmentValue: documentFragmentValue, document: document, documentValue: documentValue, documentType: documentType,
   documentTypeValue: documentTypeValue, element: element, elementValue: elementValue, text: text, textValue: textValue,
 
-  entityNode: entityNode, entityNodeValue: entityNodeValue, emptyNode: emptyNode, emptyNodeValue: emptyNodeValue, parentNode: parentNode
+  entityNode: entityNode, entityNodeValue: entityNodeValue, emptyNode: emptyNode, parentNode: parentNode
 };
 
 module.exports = schema;
--- src/schema/index.js
+++ src/schema/index.js
@@ -3,28 +3,27 @@
 const entityNode = require( 'mtype-node/schema/entityNode.schema.json' )
 const entityNodeValue = require( 'mtype-node/schema/entityNodeValue.schema.json' )
 const emptyNode = require( 'mtype-node/schema/emptyNode.schema.json' )
-const emptyNodeValue = require( 'mtype-node/schema/emptyNodeValue.schema.json' )
 const parentNode = require( 'mtype-node/schema/parentNode.schema.json' )
 
-const comment = require( './comment.schema.json' )
-const commentValue = require( './commentValue.schema.json' )
-const documentFragment = require( './documentFragment.schema.json' )
-const documentFragmentValue = require( './documentFragmentValue.schema.json' )
-const document = require( './document.schema.json' )
-const documentValue = require( './documentValue.schema.json' )
-const documentType = require( './documentType.json' )
-const documentTypeValue = require( './documentTypeValue.schema.json' )
-const element = require( './element.schema.json' )
-const elementValue = require( './elementValue.schema.json' )
-const text = require( './text.schema.json' )
-const textValue = require( './textValue.schema.json' )
+const comment = require( '../../schema/comment.schema.json' )
+const commentValue = require( '../../schema/commentValue.schema.json' )
+const documentFragment = require( '../../schema/documentFragment.schema.json' )
+const documentFragmentValue = require( '../../schema/documentFragmentValue.schema.json' )
+const document = require( '../../schema/document.schema.json' )
+const documentValue = require( '../../schema/documentValue.schema.json' )
+const documentType = require( '../../schema/documentType.json' )
+const documentTypeValue = require( '../../schema/documentTypeValue.schema.json' )
+const element = require( '../../schema/element.schema.json' )
+const elementValue = require( '../../schema/elementValue.schema.json' )
+const text = require( '../../schema/text.schema.json' )
+const textValue = require( '../../schema/textValue.schema.json' )
 
 const schema = {
   comment, commentValue, documentFragment,
   documentFragmentValue, document, documentValue, documentType,
   documentTypeValue, element, elementValue, text, textValue,
 
-  entityNode, entityNodeValue, emptyNode, emptyNodeValue, parentNode
+  entityNode, entityNodeValue, emptyNode, parentNode
 }
 
 module.exports = schema
