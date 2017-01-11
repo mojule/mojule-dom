a7a83dd9f90375cc5dd1fef034af30df8b0c50c9
Nik Coughlin <nrkn.com@gmail.com>
2017-01-11 16:04:25 +1300

    css-select needs to point at git
    As fb55 has not published to npm yet!

--- test/index.js
+++ test/index.js
@@ -9,4 +9,35 @@ describe( 'mojule-dom', () => {
 
     assert( dom )
   })
+
+  it( 'stringifies', () => {
+    const dom = Dom( '<p>Hello, World!</p>' )
+    const html = dom.stringify()
+
+    assert.equal( typeof html, 'string' )
+  })
+
+  it( 'clones', () => {
+    const dom = Dom( '<p>Hello, World!</p>' )
+    const dom2 = dom.clone()
+    const html = dom2.stringify()
+
+    assert.equal( typeof html, 'string' )
+  })
+
+  it( 'selects', () => {
+    const dom = Dom( '<p><strong>Hello</strong>, World!</p>' )
+    const strong = dom.select( 'strong' )
+
+    const html = strong.stringify()
+
+    assert.equal( typeof html, 'string' )
+  })
+
+  it( 'selects all', () => {
+    const dom = Dom( '<p><strong>Hello</strong>, <strong>World</strong>!</p>' )
+    const strongs = dom.selectAll( 'strong' )
+
+    assert.equal( strongs.length, 2 )
+  })
 })
