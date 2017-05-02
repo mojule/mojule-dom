14db11dacff583fa421d1489998b766a0380678f
Nik Coughlin <nrkn.com@gmail.com>
2017-05-02 14:24:21 +1200

    Fixes #7

--- dist/index.js
+++ dist/index.js
@@ -2,25 +2,11 @@
 
 var is = require('@mojule/is');
 var TreeFactory = require('@mojule/tree').Factory;
+var FactoryFactory = require('@mojule/tree').FactoryFactory;
 var domPlugins = require('@mojule/dom-plugins');
 var defaultPlugins = require('./plugins');
-
-var Factory = function Factory() {
-  for (var _len = arguments.length, plugins = Array(_len), _key = 0; _key < _len; _key++) {
-    plugins[_key] = arguments[_key];
-  }
-
-  var options = {};
-
-  if (plugins.length > 0 && is.object(plugins[plugins.length - 1])) options = plugins.pop();
-
-  if (plugins.length === 1 && is.array(plugins[0])) plugins = plugins[0];
-
-  plugins = domPlugins.concat(defaultPlugins).concat(plugins);
-
-  return TreeFactory(plugins, options);
-};
-
+var plugins = domPlugins.concat(defaultPlugins);
+var Factory = FactoryFactory(TreeFactory, plugins);
 var VDom = Factory();
 
 Object.assign(VDom, { Factory: Factory });
--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@
 
 const is = require( '@mojule/is' )
 const TreeFactory = require( '@mojule/tree' ).Factory
+const FactoryFactory = require( '@mojule/tree' ).FactoryFactory
 const domPlugins = require( '@mojule/dom-plugins' )
 const defaultPlugins = require( './plugins' )
-
-const Factory = ( ...plugins ) => {
-  let options = {}
-
-  if( plugins.length > 0 && is.object( plugins[ plugins.length - 1 ] ) )
-    options = plugins.pop()
-
-  if( plugins.length === 1 && is.array( plugins[ 0 ] ) )
-    plugins = plugins[ 0 ]
-
-  plugins = domPlugins.concat( defaultPlugins ).concat( plugins )
-
-  return TreeFactory( plugins, options )
-}
-
+const plugins = domPlugins.concat( defaultPlugins )
+const Factory = FactoryFactory( TreeFactory, plugins )
 const VDom = Factory()
 
 Object.assign( VDom, { Factory } )
